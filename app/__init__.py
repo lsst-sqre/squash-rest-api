b2efe31730863f96cbf60973672fe05446c493bb
Angelo Fausti <afausti@lsst.org>
2017-12-20 15:37:55 -0700

    Added job endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .resources.user import User, UserList, Register
 from .resources.metric import Metric, MetricList
 from .resources.specification import Specification, SpecificationList
 from .resources.measurement import Measurement, MeasurementList
+from .resources.job import Job, JobList
 
 
 def create_app(config):
@@ -27,9 +28,16 @@ def create_app(config):
     # register api resources
     api = Api(app)
 
-    # add api documentation
-    Swagger(app)
+    template = {"tags": [{"name": "Metrics"},
+                         {"name": "Metric Specifications"},
+                         {"name": "Metric Measurements"},
+                         {"name": "Jobs"},
+                         {"name": "Users"}]}
+    # Add api documentation
+    Swagger(app, template=template)
 
+    api.add_resource(Job, '/job/<string:ci_id>')
+    api.add_resource(JobList, '/jobs')
     api.add_resource(User, '/user/<string:username>')
     api.add_resource(UserList, '/users')
     api.add_resource(Register, '/register')
@@ -37,7 +45,7 @@ def create_app(config):
     api.add_resource(MetricList, '/metrics')
     api.add_resource(Specification, '/specification/<string:name>')
     api.add_resource(SpecificationList, '/specifications')
-    api.add_resource(Measurement, '/measurement/<string:job>')
+    api.add_resource(Measurement, '/measurement/<string:ci_id>')
     api.add_resource(MeasurementList, '/measurements')
 
     return app
