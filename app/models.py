0d17b61fc6d153f1992c8aee406c61e8d793a2a1
Angelo Fausti <afausti@lsst.org>
2017-12-20 15:35:03 -0700

    Added job and package models

--- app/models.py
+++ app/models.py
@@ -4,12 +4,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Implements JSON data type in MySQL 5.7 see
 # https://jira.lsstcorp.org/browse/DM-12191
 from sqlalchemy.dialects.mysql import JSON
+from sqlalchemy.sql import expression
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.dialects.mysql import TIMESTAMP
+
+
+# https://jira.lsstcorp.org/browse/DM-12193
+class now(expression.FunctionElement):
+    type = TIMESTAMP
+
+
+@compiles(now, 'mysql')
+def mysql_now(element, compiler, **kw):
+    return "CURRENT_TIMESTAMP()"
 
 
 class UserModel(db.Model):
     """Database model for authenticated API users."""
 
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True,
@@ -47,46 +60,41 @@ class UserModel(db.Model):
 
 
 class MetricModel(db.Model):
-    """Database model for squash metrics as defined
+    """Database model for metrics as defined
     in the lsst.verify package. See https://sqr-019.lsst.io/
-
-    name : `str`
-        Name of the metric.
-    description : `str`
-        Short description about the metric.
-    unit : `str`
-        Units of the metric. String representation of an astropy unit.
-        An empty string means an unitless quantity.
-    tags : `list`
-        Tags associated with this metric. Tags are strings
-        that can be used to group metrics.
-    reference: `dict`, optional
-        reference to the original document that defines the metric
-        usually with a handle to the document, its url and page number.
-    package: name of the package that defines this metric
     """
 
-    __tablename__ = 'metrics'
+    __tablename__ = 'metric'
 
     id = db.Column(db.Integer, primary_key=True)
-    # name of the metric
-    name = db.Column(db.String(64), nullable=False)
-    # short description about the metric
+    # Full qualified name of the metric including the package
+    # name, e.g. validate_drp.AM1
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    # Name of the package that defines this metric, e.g. validate_drp
+    package = db.Column(db.String(64))
+    # Display name of the metric, e.g. `AM1`
+    display_name = db.Column(db.String(64))
+    # Short description about the metric.
     description = db.Column(db.Text())
-    # name of the package that defines the metric
-    package = db.Column(db.String(64), nullable=False)
-    # units of the metric
+    # Units of the metric. String representation of an astropy unit.
+    # An empty string means an unitless quantity.
     unit = db.Column(db.String(16))
-    # tags associated with the metric
+    # Tags associated with this metric. Tags are strings
+    # that can be used to group metrics.
     tags = db.Column(JSON())
-    # reference to the original doc that defines the metric
+    # Reference to the original document that defines the metric
+    # usually with a handle to the document, its url and page number.
     reference = db.Column(JSON())
 
     specification = db.relationship('SpecificationModel')
+    measurement = db.relationship('MeasurementModel')
 
-    def __init__(self, name, description, package, unit=None,
-                 tags=None, reference=None):
+    def __init__(self, name, package, display_name, description,
+                 unit=None, tags=None, reference=None):
+
         self.name = name
+        self.package = package
+        self.display_name = display_name
         self.description = description
         self.package = package
         self.unit = unit
@@ -95,19 +103,16 @@ class MetricModel(db.Model):
 
     def json(self):
         return {'name': self.name,
-                'description': self.description,
                 'package': self.package,
+                'display_name': self.display_name,
+                'description': self.description,
                 'unit': self.unit,
                 'tags': self.tags,
                 'reference': self.reference}
 
     @classmethod
     def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).all()
-
-    @classmethod
-    def find_by_fqn(cls, package, name):
-        return cls.query.filter_by(package=package, name=name).first()
+        return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
         db.session.add(self)
@@ -119,45 +124,32 @@ class MetricModel(db.Model):
 
 
 class SpecificationModel(db.Model):
-    """Database model for metrics specifications as defined
+    """Database model for metric specifications as defined
     in the lsst.verify package. See https://sqr-019.lsst.io/
-
-    id: `int`
-        Id of the specification
-    name : `str`
-        Name of the specification
-    metric_id : `integer`
-        Id of the metric this specification applies to
-    threshold : `dict`
-        defines the specification, usually the threshold
-        field has an operator, value and unit keys.
-    tags : `list`
-        Tags associated with this specification
-    metadata_query: `dict`
-        Additional metadata information that can be tested
-        against the job metadata
     """
 
-    __tablename__ = 'specifications'
+    __tablename__ = 'spec'
 
-    # id of the specification
     id = db.Column(db.Integer, primary_key=True)
-    # name of the specification
+    # Full qualified name of the specification, e.g.
+    # validate_drp.AM1.minimum_gri
     name = db.Column(db.String(64), nullable=False)
-    # id of the metric this specification applies to
-    metric_id = db.Column(db.Integer, db.ForeignKey('metrics.id'))
-    # defines the specification test
+    # Defines the specification, usually the threshold
+    # field has an operator, value and unit keys.
     threshold = db.Column(JSON())
-    # tags associated with this specification
+    # Tags associated with this specification
     tags = db.Column(JSON())
-    # additional metadata information
+    # Additional metadata information that can be tested
+    # against the job metadata
     metadata_query = db.Column(JSON())
+    # Id of the metric this specification applies to
+    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'))
 
     def __init__(self, name, metric_id, threshold=None, tags=None,
                  metadata_query=None):
 
         self.name = name
-        self.metric_id = metric_id
+        self.metric = metric_id
         self.threshold = threshold
         self.tags = tags
         self.metadata_query = metadata_query
@@ -181,40 +173,148 @@ class SpecificationModel(db.Model):
         db.session.commit()
 
 
-class MeasurementModel(db.Model):
-    """Database model for measurements of squash metrics.
-
-    This implementation provides a minimal model to test the
-    JSON data type.
+class JobModel(db.Model):
+    """Database model for lsst.verify metadata
     """
 
-    __tablename__ = 'measurements'
+    __tablename__ = 'job'
+
+    STATUS_OK = 0
+    STATUS_FAILED = 1
 
     id = db.Column(db.Integer, primary_key=True)
-    job = db.Column(db.String(80))
-    value = db.Column(db.Float())
-    data = db.Column(JSON())
+    # ID of the CI run
+    ci_id = db.Column(db.String(32), nullable=False, unique=True)
+    # Name of the package than runs through CI, e.g. validate_drp
+    ci_name = db.Column(db.String(64), nullable=False)
+    # Name of the dataset, e.g cfht
+    ci_dataset = db.Column(db.String(32), nullable=False)
+    # Name of the platform where the the CI runs, e.g centos-7
+    ci_label = db.Column(db.String(64))
+    # URL to access the results of the CI run
+    ci_url = db.Column(db.Unicode(255))
+    # Status of this run 0=ok, 1=failed
+    status = db.Column(db.Integer, default=STATUS_OK)
 
-    def __init__(self, metric_name, package, job, value, data):
+    # Timestamp when the lsst.verify job was created
+    date_created = db.Column(db.TIMESTAMP, nullable=False,
+                             server_default=now())
 
-        metric = MetricModel.find_by_fqn(package, metric_name)
+    # URL for the git lfs repo for the dataset used in this run
+    dataset_repo_url = db.Column(db.Unicode(255))
+    # Name of the instrument that collected the dataset used in this run
+    instrument = db.Column(db.String(64))
+    # Name of the filter associated to this dataset
+    filter_name = db.Column(db.String(16))
 
-        if metric:
-            self.metric_id = metric.id
-        else:
-            return {"message": "Metric '{}.{}' not "
-                               "found.".format(package, metric_name)}, 404
+    # Measurements created by this job, are also deleted
+    # upon job deletion
+    measurements = db.relationship('MeasurementModel', lazy='dynamic',
+                                   cascade="all, delete-orphan")
 
-        self.job = job
-        self.value = value
-        self.data = data
+    def __init__(self, ci_id,
+                 ci_name="",
+                 ci_dataset="",
+                 ci_label="",
+                 ci_url="",
+                 dataset_repo_url="",
+                 instrument="",
+                 filter_name=""):
+
+        self.ci_id = ci_id
+        self.ci_name = ci_name
+        self.ci_dataset = ci_dataset
+        self.ci_label = ci_label
+        self.ci_url = ci_url
+        self.dataset_repo_url = dataset_repo_url
+        self.instrument = instrument
+        self.filter_name = filter_name
 
     def json(self):
-        return {'job': self.job, 'value': self.value, 'data': self.data}
+        return {'ci_id': self.ci_id,
+                'measurements': [measurement.json() for measurement
+                                 in self.measurements.all()]}
+    def json_summary(self):
+        return {'ci_id': self.ci_id}
 
     @classmethod
-    def find_by_job(cls, job):
-        return cls.query.filter_by(job=job).first()
+    def find_by_ci_id(cls, ci_id):
+        return cls.query.filter_by(ci_id=ci_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class PackageModel(db.Model):
+    """A specific version of an eups package used
+    by the lsst.verify job"""
+
+    __tablename__ = 'package'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # EUPS package name
+    name = db.Column(db.String(64), nullable=False)
+    # URL of the git repository for this package
+    git_url = db.Column(db.Unicode(255), unique=True)
+    # SHA1 hash of the git commit
+    git_commit = db.Column(db.String(64), nullable=False)
+    # Resolved git branch that the commit resides on
+    git_branch = db.Column(db.String(64))
+    # EUPS build version
+    build_version = db.Column(db.String(64))
+
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
+
+    def __init__(self, name, git_url, git_commit, git_branch,
+                 build_version):
+
+        self.name = name
+        self.git_url = git_url
+        self.git_commit = git_commit
+        self.git_branch = git_branch
+        self.build_version = build_version
+
+    @classmethod
+    def find_by_job(cls, job_id):
+        return cls.query.filter_by(job_id=job_id).all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class MeasurementModel(db.Model):
+    """Database model for metric measurements"""
+
+    __tablename__ = 'measurement'
+
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float)
+
+    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'))
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
+
+    def __init__(self, job_id, metric_id, value=None):
+
+        self.job_id = job_id
+        self.metric_id = metric_id
+        self.value = value
+
+    def json(self):
+        return {'value': self.value}
+
+    @classmethod
+    # A job can have multiple measurements
+    def find_by_job_id(cls, job_id):
+        return cls.query.filter_by(job_id=job_id).all()
 
     def save_to_db(self):
         db.session.add(self)
