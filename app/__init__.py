5fc59e611132e2dfe881f5fee6e2ac2fcf65f74c
Angelo Fausti <afausti@lsst.org>
2017-12-29 16:03:59 -0700

    Added job and jenkins resources

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,8 @@ from .resources.user import User, UserList, Register
 from .resources.metric import Metric, MetricList
 from .resources.specification import Specification, SpecificationList
 from .resources.measurement import Measurement, MeasurementList
-from .resources.job import Job, JobList
+from .resources.job import Job, Job_
+from .resources.jenkins import Jenkins
 
 
 def create_app(config):
@@ -28,24 +29,40 @@ def create_app(config):
     # register api resources
     api = Api(app)
 
-    template = {"tags": [{"name": "Metrics"},
+    template = {"tags": [{"name": "Jobs"},
+                         {"name": "Metrics"},
                          {"name": "Metric Specifications"},
                          {"name": "Metric Measurements"},
-                         {"name": "Jobs"},
                          {"name": "Users"}]}
     # Add api documentation
     Swagger(app, template=template)
 
-    api.add_resource(Job, '/job/<string:ci_id>')
-    api.add_resource(JobList, '/jobs')
+    # Generic Job resource
+    api.add_resource(Job, '/job')
+    # Because flasgger cannot handle multiple resource endpoints,
+    # the methods that require the job_id parameter are implemented
+    # in a separate resource, see the status of this issue at
+    # https://github.com/rochacbruno/flasgger/issues/174
+    api.add_resource(Job_, '/job/<int:job_id>')
+
+    # Resource for jobs in the jenkins enviroment
+    api.add_resource(Jenkins, '/jenkins/<string:ci_id>')
+
+    # User resources
     api.add_resource(User, '/user/<string:username>')
     api.add_resource(UserList, '/users')
     api.add_resource(Register, '/register')
+
+    # Metric resources
     api.add_resource(Metric, '/metric/<string:name>')
     api.add_resource(MetricList, '/metrics')
-    api.add_resource(Specification, '/specification/<string:name>')
-    api.add_resource(SpecificationList, '/specifications')
-    api.add_resource(Measurement, '/measurement/<string:ci_id>')
+
+    # Metric specifications resources
+    api.add_resource(Specification, '/spec/<string:name>')
+    api.add_resource(SpecificationList, '/specs')
+
+    # Metric measurement resources
+    api.add_resource(Measurement, '/measurement/<int:job_id>')
     api.add_resource(MeasurementList, '/measurements')
 
     return app
