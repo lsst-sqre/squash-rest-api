c076f163f73cb29d9ed9812c76690c5cb51cd47c
Angelo Fausti <afausti@lsst.org>
2018-01-04 17:11:28 -0700

    Run the flask app with uwsgi in prodution

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ To run in development mode:
 
 In production mode, the kubernetes configuration will set the env variable:
 
-  export SQUASH_API_CONFIG=app.config.Production
+  export SQUASH_API_PROFILE=app.config.Production
 
 and the app will run under the uwsgi server calling this script:
 
@@ -22,18 +22,17 @@ import os
 from app import create_app, db
 from app.models import UserModel
 
+profile = os.environ.get('SQUASH_API_PROFILE', 'app.config.Development')
+app = create_app(profile)
+
+with app.app_context():
+    db.create_all()
+
+    # create admin user
+    if UserModel.query.get(1) is None:
+        user = UserModel(username=app.config['DEFAULT_USER'],
+                         password=app.config['DEFAULT_PASSWORD'])
+        user.save_to_db()
+
 if __name__ == '__main__':
-
-    profile = os.environ.get('SQUASH_API_PROFILE', 'app.config.Development')
-    app = create_app(profile)
-
-    with app.app_context():
-        db.create_all()
-
-        # create admin user
-        if UserModel.query.get(1) is None:
-            user = UserModel(username=app.config['DEFAULT_USER'],
-                             password=app.config['DEFAULT_PASSWORD'])
-            user.save_to_db()
-
     app.run()
